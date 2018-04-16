ee171753096825794cad879a46027fcd4e2a134d
sdbowen <sdbowen.git@gmail.com>
2018-04-15 21:58:59 -0500

    Added campground edit

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -4,9 +4,10 @@ var express = require('express'),
   mongoose = require('mongoose'),
   passport = require('passport'),
   LocalStrategy = require('passport-local'),
+  methodOverride = require('method-override')
   Campground = require('./models/campground'),
   Comment = require('./models/comment'),
-  User = require('./models/user'),
+  User = require ('./models/user'),
   seedDB = require('./seeds');
 
 // Require routes
@@ -18,6 +19,7 @@ mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 
 // Seed database
 //seedDB();
--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -58,6 +58,28 @@ router.get('/:id', function(req, res) {
     });
 });
 
+// Edit campground
+router.get('/:id/edit', function(req, res) {
+  Campground.findById(req.params.id, function(err, selectedCampground) {
+    if (err) {
+      res.redirect('/campgrounds');
+    } else {
+      res.render('campgrounds/edit', { campground: selectedCampground });
+    }
+  });
+});
+
+// Update campground
+router.put('/:id', function(req, res) {
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
+    if (err) {
+      res.redirect('/campgrounds');
+    } else {
+      res.redirect('/campgrounds/' + req.params.id);
+    }
+  })
+})
+
 // Check if user is logged in
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
