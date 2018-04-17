5ace9d30ac7c0f343ae7d151a8e261b552b81010
sdbowen <sdbowen.git@gmail.com>
2018-04-16 22:09:23 -0500

    Show/hide comment buttons per user

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -39,7 +39,7 @@ router.post('/', isLoggedIn, function(req, res) {
 });
 
 // Handle comment edit
-router.get('/:comment_id/edit', function(req, res) {
+router.get('/:comment_id/edit', checkCommentOwner, function(req, res) {
   Comment.findById(req.params.comment_id, function(err, selectedComment) {
     if (err) {
       res.redirect('back');
@@ -53,7 +53,7 @@ router.get('/:comment_id/edit', function(req, res) {
 });
 
 // Handle update comment
-router.put('/:comment_id', function(req, res) {
+router.put('/:comment_id', checkCommentOwner, function(req, res) {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
     if(err) {
       res.redirect('back');
@@ -64,7 +64,7 @@ router.put('/:comment_id', function(req, res) {
 });
 
 // Handle comment destroy
-router.delete('/:comment_id', function(req, res) {
+router.delete('/:comment_id', checkCommentOwner, function(req, res) {
   Comment.findByIdAndRemove(req.params.comment_id, function(err) {
     if (err) {
       res.redirect('backS');
@@ -80,6 +80,24 @@ function isLoggedIn(req, res, next) {
     return next();
   }
   res.redirect('/login');
+}
+
+function checkCommentOwner(req, res, next) {
+  if (req.isAuthenticated()) {
+    Campground.findById(req.params.comment_id, function(err, selectedComment) {
+      if (err) {
+        res.redirect('back');
+      } else {
+        if (selectedComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect('back');
+        }
+      }
+    });
+  } else {
+    res.redirect('back');
+  }
 }
 
 module.exports = router;
