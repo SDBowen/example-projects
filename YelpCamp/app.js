52fb71879e02d368a7280989a771395f6a678502
sdbowen <sdbowen.git@gmail.com>
2018-04-28 10:50:38 -0500

    Remove hard set PORT in app.listen

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -52,6 +52,7 @@ app.use('/', indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(3000, 'localhost', function() {
-  console.log('The YelpCamp server has started!');
-});
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("YelpCamp server has started!");
+})
