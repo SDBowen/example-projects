71668c57f6b768173931a0af73a14f50bb4f8077
sdbowen <sdbowen.git@gmail.com>
2018-05-16 18:46:07 -0500

    Add arrow functions

--- Concept Examples/Fetch/app.js
+++ Concept Examples/Fetch/app.js
@@ -5,52 +5,40 @@ document.getElementById('button3').addEventListener('click', getExternal);
 // Get local text file data
 function getText() {
   fetch('test.txt')
-    .then(function(response) {
-      return response.text();
-    })
-    .then(function(data) {
+    .then(response => response.text())
+    .then(data => {
       console.log(data);
       document.getElementById('output').innerHTML = data;
     })
-    .catch(function(err) {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 }
 
 // Get local json data
 function getJson() {
-    fetch('posts.json')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        console.log(data);
-        let output = '';
-        data.forEach(function(post) {
-            output += `<li>${post.title}</li>`
-        });
-        document.getElementById('output').innerHTML = output;
-      })
-      .catch(function(err) {
-        console.log(err);
+  fetch('posts.json')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      let output = '';
+      data.forEach(function(post) {
+        output += `<li>${post.title}</li>`;
       });
-  }
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(err => console.log(err));
+}
 
-  // Get from external API
+// Get from external API
 function getExternal() {
-    fetch('https://api.github.com/users')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        console.log(data);
-        let output = '';
-        data.forEach(function(user) {
-            output += `<li>${user.login}</li>`
-        });
-        document.getElementById('output').innerHTML = output;
-      })
-      .catch(function(err) {
-        console.log(err);
+  fetch('https://api.github.com/users')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      let output = '';
+      data.forEach(function(user) {
+        output += `<li>${user.login}</li>`;
       });
-  }
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(err => console.log(err));
+}
