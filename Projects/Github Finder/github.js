627aee35aee5950940c04fdcea65c412ea7559c3
sdbowen <sdbowen.git@gmail.com>
2018-05-17 20:03:58 -0500

    Complete demo

--- Projects/Github Finder/github.js
+++ Projects/Github Finder/github.js
@@ -2,6 +2,8 @@ class GitHub {
   constructor() {
     this.client_id = userID;
     this.client_seceret = userSeceret;
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
@@ -11,10 +13,20 @@ class GitHub {
       }&client_seceret=${this.client_seceret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_seceret=${
+        this.client_seceret
+      }`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-        profile
-    }
+      profile,
+      repos
+    };
   }
 }
