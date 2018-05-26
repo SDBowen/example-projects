936a64493714d781420688c9c85f1aefed50df57
sdbowen <sdbowen.git@gmail.com>
2018-05-26 12:41:19 -0500

    Add python test

--- Python/app.py
+++ Python/app.py
@@ -1,11 +1,41 @@
 #! python3
 import pyautogui
+import time
 
-distance = 200
-while distance > 0:
-    pyautogui.dragRel(distance, 0, duration=0.5)   # move right
-    distance -= 5
-    pyautogui.dragRel(0, distance, duration=0.5)   # move down
-    pyautogui.dragRel(-distance, 0, duration=0.5)  # move left
-    distance -= 5
-    pyautogui.dragRel(0, -distance, duration=0.5)  # move up
+# Delay start
+
+time.sleep(5)
+
+# Detect screen resolution
+
+# Detect if program is opn
+
+# Focus on program and maximize
+
+# Select menu search
+pyautogui.hotkey('ctrl', 'm')
+# Select menu
+pyautogui.typewrite('13.12.1')
+pyautogui.press('enter')
+
+# Fill out menu
+# Site
+pyautogui.typewrite('370')
+pyautogui.press('enter')
+# Cost Set
+pyautogui.typewrite('CURRENT')
+pyautogui.press('enter')
+# Item (From: and To:)
+pyautogui.typewrite('TEST')
+pyautogui.press('tab')
+pyautogui.typewrite('TEST')
+# Tab to next input field
+for _ in range(11):
+    pyautogui.press('tab')
+# Freeze or Unfreeze
+pyautogui.press('f')
+pyautogui.press('enter')
+# Submit form
+pyautogui.press('enter')
+# Check for menu run completion
+
