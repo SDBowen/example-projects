cbaca77852b18cfa3c590895691fae41f8c73ad4
sdbowen <sdbowen.git@gmail.com>
2018-05-28 17:48:15 -0500

    Add local storage update and delete

--- TrackCalorieApp/app.js
+++ TrackCalorieApp/app.js
@@ -34,6 +34,31 @@ const StorageCtrl = (function(){
             }
             return items;
         },
+
+        updateItemStorage: function(updatedItem) {
+            let items = JSON.parse(localStorage.getItem('items'));
+
+            items.forEach(function (item, index) {
+                if (updatedItem.id === item.id) {
+                    items.splice(index, 1, updatedItem);
+                } 
+            });
+            localStorage.setItem('items', JSON.stringify(items));
+        },
+
+        deleteItemFromStorage: function (id) {
+            let items = JSON.parse(localStorage.getItem('items'));
+
+            items.forEach(function (item, index) {
+                if (id === item.id) {
+                    items.splice(index, 1);
+                } 
+            localStorage.setItem('items', JSON.stringify(items));
+            });
+        },
+        clearItemsFromStorage: function () {
+            localStorage.removeItem('items');
+        }
     }
 })();
 
@@ -391,6 +416,9 @@ const App = (function (ItemCtrl, StorageCtrl, UICtrl) {
         // Add total calories to UI
         UICtrl.showTotalCalories(totalCalories);
 
+        // Update local storage
+        StorageCtrl.updateItemStorage(updatedItem);
+
         UICtrl.clearEditState();
 
         e.preventDefault();
@@ -413,6 +441,9 @@ const App = (function (ItemCtrl, StorageCtrl, UICtrl) {
         // Add total calories to UI
         UICtrl.showTotalCalories(totalCalories);
 
+        // Delete from local storage
+        StorageCtrl.deleteItemFromStorage(currentItem.id);
+
         UICtrl.clearEditState();
         
         e.preventDefault();
@@ -431,6 +462,9 @@ const App = (function (ItemCtrl, StorageCtrl, UICtrl) {
 
         // Remove from UI
         UICtrl.removeItems();
+
+        // Remove from local storage
+        StorageCtrl.clearItemsFromStorage();
 
         // Hide list
         UICtrl.hideList();
