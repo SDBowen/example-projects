4391acb89d0cb62fb8ee5551b23040990299dfbf
sdbowen <sdbowen.git@gmail.com>
2018-06-04 20:00:20 -0500

    Add course progress

--- VidJot/app.js
+++ VidJot/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -17,6 +18,10 @@ require('./models/Idea');
 
 const Idea = mongoose.model('idea');
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Handlebars middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -32,6 +37,49 @@ app.get('/', (req, res) => {
 // About Route
 app.get('/about', (req, res) => {
   res.render('about');
+});
+
+// Idea index page
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+    .sort({ date: 'desc' })
+    .then(ideas => {
+      res.render('ideas/index', { ideas });
+    });
+});
+
+// Add idea form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+// Process form
+app.post('/ideas', (req, res) => {
+  const errors = [];
+
+  if (!req.body.title) {
+    errors.push({ text: 'Please add a title' });
+  }
+
+  if (!req.body.details) {
+    errors.push({ text: 'Please add some details' });
+  }
+
+  if (errors.length > 0) {
+    res.render('ideas/add', {
+      errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    };
+    new Idea(newUser).save().then(idea => {
+      res.redirect('/ideas');
+    });
+  }
 });
 
 const port = 5000;
