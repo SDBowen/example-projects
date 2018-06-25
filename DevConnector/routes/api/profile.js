3c4344e95cfa9571c03244905f5d367dfc899c98
sdbowen <sdbowen.git@gmail.com>
2018-06-24 19:09:13 -0500

    Add education and experience routes and validation

--- DevConnector/routes/api/profile.js
+++ DevConnector/routes/api/profile.js
@@ -10,6 +10,8 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 // Load validation
 const validatorProfileInput = require('../../validation/profile');
+const validatorExperienceInput = require('../../validation/experience');
+const validatorEducationInput = require('../../validation/education');
 
 // @Route   GET api/profile/test
 // @Desc    Test profile route
@@ -160,6 +162,74 @@ router.post(
           new Profile(profileFields).save().then(profile => res.json(profile));
         });
       }
+    });
+  }
+);
+
+// @Route   POST api/profile/experience
+// @Desc    Add experience to profile
+// @Access  Private
+router.post(
+  '/experience',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatorExperienceInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      // Add to experience array
+      profile.experience.unshift(newExp);
+
+      profile.save().then(returnProfile => res.json(returnProfile));
+    });
+  }
+);
+
+// @Route   POST api/profile/education
+// @Desc    Add education to profile
+// @Access  Private
+router.post(
+  '/education',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatorEducationInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldofstudy: req.body.fieldofstudy,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      // Add to experience array
+      profile.education.unshift(newEdu);
+
+      profile.save().then(returnProfile => res.json(returnProfile));
     });
   }
 );
