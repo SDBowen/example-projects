af55974ac05290b3381746225e36a82c42672387
sdbowen <sdbowen.git@gmail.com>
2018-07-05 11:58:32 -0500

    Add starting component files

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,1 +1,17 @@
-test;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { apiKey } from './keys';
+
+import SearchBar from './components/search_bar';
+
+// Create component that will produce HTML
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+// Display component on DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
