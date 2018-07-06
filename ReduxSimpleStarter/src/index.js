ad7ac7f0ec5e8d1fc73ad6b94dee0c92fb3ee7fd
sdbowen <sdbowen.git@gmail.com>
2018-07-05 22:28:29 -0500

    Complete components and add styling

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { apiKey } from './keys';
 import YTSearch from 'youtube-api-search';
-
+import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 
 // Create component that will produce HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('cats');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: apiKey, term: term }, videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+        />
+      </div>
+    );
+  }
+}
 
 // Display component on DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
