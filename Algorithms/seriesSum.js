e0c3e458845b9490cbbc8ae5422385ecb3db3dde
sdbowen <sdbowen.git@gmail.com>
2019-02-26 12:48:21 -0700

    Refactor series sum to remove not needed code

--- Algorithms/seriesSum.js
+++ Algorithms/seriesSum.js
@@ -1,11 +1,9 @@
 function SeriesSum(n) {
-  let base = 1;
   let sum = 0.0;
 
   for (let i = 0; i < n; i += 1) {
-    sum += 1 / base;
-    base += 3;
+    sum += 1 / (1 + i * 3);
   }
 
-  return sum.toFixed(2).toString();
+  return sum.toFixed(2);
 }
