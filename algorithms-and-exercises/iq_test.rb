f9c8089c9a5a0c84797dad5abc1c0973dfeb2fc6
sdbowen <sdbowen.git@gmail.com>
2019-03-05 11:44:46 -0700

    Add updated solution

--- algorithms-and-exercises/iq_test.rb
+++ algorithms-and-exercises/iq_test.rb
@@ -1,7 +1,9 @@
 def iq_test(numbers)
-  numbers_array = numbers.split(' ').map(&:to_h)
+  even_or_odd = numbers.split(' ').map(&:to_i).map(&:even?)
 
-  print numbers_array
-
-  # even_or_odd = numbers_array.map(&:even?)
+  count = Hash.new(0)
+  even_or_odd.each { |value| count[value] += 1 }
+  
+  return even_or_odd.index(count.key(1)) + 1
 end
+
